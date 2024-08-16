0e9ffbad997c3a7dc9b1a5c9a1c1b8ab0ac4ce63
cloudsolutionstech <omadetech@gmai.com>
2024-08-16 16:45:28 -0400

    Added vpc variables

--- vpc-variables.tf
+++ vpc-variables.tf
@@ -1,25 +1,3 @@
-# Input variable for AWS region
-variable "aws_region" {
-  description = "AWS region where resources will be created"
-  type        = string
-  default     = "us-east-1"
-}
-
-# Input variable for environment
-variable "environment" {
-  description = "Environment name to use as a prefix"
-  type        = string
-  default     = "prod"
-}
-
-# Input variable for business division
-variable "business_division" {
-  description = "Business division within the organization"
-  type        = string
-  default     = "Admin"
-}
-cloud@cloudsolutions MINGW64 ~/Desktop/aws-vpc (main)
-$ cat vpc-variables.tf
 # VPC Name variable
 variable "vpc_name" {
   description = "A name for the VPC"
